1f71356008b76b01a0d70203c6a394c6f6e48a52
Yan <laptevyan@mail.ru>
2023-07-09 14:28:14 +0300

    last

--- src/main/java/com/example/newMock/Controller/MainController.java
+++ src/main/java/com/example/newMock/Controller/MainController.java
@@ -2,10 +2,14 @@ package com.example.newMock.Controller;
 
 
 import com.example.newMock.Model.RequestDTO;
+import com.example.newMock.Model.ResponseDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,17 +23,83 @@ public class MainController {
     private Logger log = LoggerFactory.getLogger(MainController.class);
 
     ObjectMapper mapper = new ObjectMapper();
+
     @PostMapping(
             value = "/info/postBalances",
             produces = MediaType.APPLICATION_JSON_VALUE,
             consumes = MediaType.APPLICATION_JSON_VALUE
     )
-    public Object postBalances(@RequestBody RequestDTO requestDTO){
-        try{
+    public Object postBalances(@RequestBody RequestDTO requestDTO) {
+        try {
             String clientId = requestDTO.getClientId();
             char firstDigit = clientId.charAt(0);
             BigDecimal maxLimit;
-            String my = "123";
+
+            if (firstDigit == '8') {
+                maxLimit = new BigDecimal(2000.00);
+            } else if (firstDigit == '9') {
+                maxLimit = new BigDecimal(1000.00);
+            } else {
+                maxLimit = new BigDecimal(10000.00);
+            }
+
+            String RqUID = requestDTO.getRqUID();
+
+            ResponseDTO responseDTO = new ResponseDTO();
+
+            responseDTO.setRqUID(RqUID);
+            responseDTO.setClientId(clientId);
+            responseDTO.setAccount(requestDTO.getAccount());
+            responseDTO.setCurrency("RUB");
+            responseDTO.setBalance("900");
+            responseDTO.setMaxLimit(maxLimit);
+
+
+            log.error("***** Запрос *****" + mapper.writerWithDefaultPrettyPrinter().writeValueAsString(requestDTO));
+            log.error("***** Запрос Ответ" + mapper.writerWithDefaultPrettyPrinter().writeValueAsString(responseDTO));
+            return responseDTO;
+
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    @GetMapping(
+            value = "/info/getBalances",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Object getBalances(@RequestBody RequestDTO requestDTO) {
+        try {
+            String clientId = requestDTO.getClientId();
+            char firstDigit = clientId.charAt(0);
+            BigDecimal maxLimit;
+
+            if (firstDigit == '8') {
+                maxLimit = new BigDecimal(2000.00);
+            } else if (firstDigit == '9') {
+                maxLimit = new BigDecimal(1000.00);
+            } else {
+                maxLimit = new BigDecimal(10000.00);
+            }
+
+            String RqUID = requestDTO.getRqUID();
+
+            ResponseDTO responseDTO = new ResponseDTO();
+
+            responseDTO.setRqUID(RqUID);
+            responseDTO.setClientId(clientId);
+            responseDTO.setAccount(requestDTO.getAccount());
+            responseDTO.setCurrency("RUB");
+            responseDTO.setBalance("900");
+            responseDTO.setMaxLimit(maxLimit);
+
+
+            log.error("***** Запрос *****" + mapper.writerWithDefaultPrettyPrinter().writeValueAsString(requestDTO));
+            log.error("***** Запрос Ответ" + mapper.writerWithDefaultPrettyPrinter().writeValueAsString(responseDTO));
+            return responseDTO;
+
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
     }
 }
--- src/main/java/com/example/newMock/Model/ResponseDTO.java
+++ src/main/java/com/example/newMock/Model/ResponseDTO.java
@@ -2,6 +2,8 @@ package com.example.newMock.Model;
 
 import lombok.*;
 
+import java.math.BigDecimal;
+
 @Getter
 @Setter
 @ToString
@@ -14,5 +16,5 @@ public class ResponseDTO {
     private String account ;
     private String currency;
     private String balance ;
-    private String maxLimit ;
+    private BigDecimal maxLimit ;
 }
